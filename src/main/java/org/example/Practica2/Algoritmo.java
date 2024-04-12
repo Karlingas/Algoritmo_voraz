2f933a0f9a790ddab9237dae0e1bc4d317c4e5fe
Karlingas <dgdcuenca@gmail.com>
2024-04-12 18:57:30 +0200

    Inicio Practica 2

--- src/main/java/org/example/Practica2/Algoritmo.java
+++ src/main/java/org/example/Practica2/Algoritmo.java
@@ -5,12 +5,21 @@ import java.util.ArrayList;
 
 public class Algoritmo {
 
-    public static Mercancia[] algoDinamico(ArrayList<Mercancia> mercancias, Camion camion){
+    public static Mercancia[] algoDinamico(Mercancia[] candidatos, Camion camion){
         Camion camionDi = new Camion(camion.getCapacidad());
-
+        Mercancia[][] tabla = tablaDecisones(candidatos);
 
 
 
         return new Mercancia[0];
     }
+
+    //Como el valor por kg es estatico no hace galta que pasemos el valor, solo el peso
+    public static Mercancia[][] tablaDecisones(Mercancia[] candidatos){
+
+
+
+        return new Mercancia[0][0];
+    }
+
 }
