3acdcbabe5e75a982acb06393afc6e7be58bc9ee
Karlingas <dgdcuenca@gmail.com>
2024-04-12 18:40:04 +0200

    Inicio Practica 2

--- src/main/java/org/example/Practica2/Algoritmo.java
+++ src/main/java/org/example/Practica2/Algoritmo.java
@@ -1,8 +1,15 @@
 package org.example.Practica2;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Algoritmo {
 
-    public static Mercancia[] algoDinamico(){
+    public static Mercancia[] algoDinamico(ArrayList<Mercancia> mercancias, Camion camion){
+        Camion camionDi = new Camion(camion.getCapacidad());
+
+
+
 
         return new Mercancia[0];
     }
--- src/main/java/org/example/Practica2/Main.java
+++ src/main/java/org/example/Practica2/Main.java
@@ -1,8 +1,20 @@
 package org.example.Practica2;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args){
 
+
+        Mercancia[] mercancias = new Mercancia[10];
+
+
+        ArrayList<Mercancia> mercanciasArray = new ArrayList<>();
+        for (int i = 0; i<mercancias.length;i++){
+            mercanciasArray.add(i,mercancias[i]);
+        }
+        Camion camion = new Camion();
+        mercancias = Algoritmo.algoDinamico(mercanciasArray,camion);
     }
 }
