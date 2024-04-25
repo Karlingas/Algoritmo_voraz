936e0a1d892659657065d6092a303749a93d831d
Marcos-Wth <166847885+Marcos-Wth@users.noreply.github.com>
2024-04-25 19:31:55 +0200

    Update Algoritmo.java

--- src/main/java/org/example/Practica2/Algoritmo.java
+++ src/main/java/org/example/Practica2/Algoritmo.java
@@ -36,11 +36,61 @@ public class Algoritmo {
     }
 
     //Como el valor por kg es estatico no hace galta que pasemos el valor, solo el peso
-    public static int[][] tablaDecisones(ArrayList<Mercancia> candidatos){
+     public static double[][] tablaDecisones(Mercancia[] candidatos, Camion camion){
+        double total= camion.getCapacidad();
+        int columnas= camion.getCapacidad()+1;
+        int filas=candidatos.length;
+        double acum=0;
+        double[][] tabla= new double[columnas][filas];
+        double[][] vector=new double[columnas+1][2];
+        int a =0;
+        for (int i = 0; i < columnas; i++) {
+            for (int j = 0; j < filas; j++) {
+                if (((candidatos[j].getPeso()+acum)<=i) && (candidatos[j].usado==false)) {
+                    tabla[i][j] = (candidatos[j].getPeso() + acum);
+                    vector[a][1]=candidatos[j].getPeso();
+                    vector[a][0]=j;
+                    a++;
+                    System.out.println("i: " + i + " a: "+a);
+                    acum = acum + candidatos[j].getPeso();
+                    candidatos[j].setUsado(true);
+                }else {
+                    for (int k = a; k >= 0; k--) {
+                        if (candidatos[j].getPeso()+(acum-vector[k][1])>acum && (candidatos[j].getPeso()+(acum-vector[k][1])<=i) && (candidatos[j].usado)==false){
+                            acum=acum-vector[k][1];
+                            tabla[i][j]=candidatos[j].getPeso()+acum;
+                            candidatos[(int)(vector[k][0])].setUsado(false);
+                            vector[k][1]=candidatos[j].getPeso();
+                            vector[k][0]=j;
+                            System.out.println("i: "+ i +" Ahora a: "+a);
+                            acum=acum+vector[k][1];
+                            candidatos[j].setUsado(true);
+                            break;
+                        }
+                    }
+                }
+                if(tabla[i][j]==0){
+                     if (j==0 && i!=0){
+                         tabla[i][j]=tabla[i-1][filas-1];
+                     }else if(j==0 && i==0){
+                         tabla[i][j]=0;
+                     }else{
+                         tabla[i][j]=tabla[i][j-1];
+                     }
+                }
+            }
+            total=total-tabla[i][filas-1];
+        }
 
+        //Esta parte es para ver si funciona
+        for (int i = 0; i < columnas; i++) {
+            for (int j = 0; j < filas; j++) {
+                System.out.println(tabla[i][j]+"  ");
+            }
+            System.out.println("\n");
+        }
+        //hasta aqui
 
-
-        return new int[0][0];
+        return tabla;
     }
-
 }
